dd3cb63983778f5058e8cc081f895cba40e6257f
Hyeon Kim <simnalamburt@gmail.com>
2014-12-13 05:08:36 +0900

    Implement dummy lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,32 @@
-#[experimental]
-pub fn lex<T: Reader>(input: &mut T) {
-    println!("{}", input.read_to_string().unwrap());
+#![experimental]
+
+struct Lexer<'a> {
+    input: Box<Buffer + 'a>
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: Box<Buffer + 'a>) -> Lexer<'a> {
+        Lexer { input: input }
+    }
+}
+
+impl<'a> Iterator<String> for Lexer<'a> {
+    fn next(&mut self) -> Option<String> {
+        match self.input.read_char() {
+            Ok(ch) => Some(ch.to_string()),
+            Err(_) => None
+        }
+    }
 }
 
 #[test]
 fn test_lex() {
     let string = "Hello, world!".to_string();
 
-    let mut reader = std::io::MemReader::new(string.into_bytes());
-    lex(&mut reader);
+    let reader = std::io::MemReader::new(string.into_bytes());
+    let mut lexer = Lexer::new(box reader);
+
+    for token in lexer {
+        println!("{}", token);
+    }
 }
