fef7e34638b72f4634cfb928b5f424908a1873ab
Hyeon Kim <simnalamburt@gmail.com>
2014-12-14 08:21:59 +0900

    Implement properly working lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,42 @@
 #![experimental]
 
-struct Lexer<'a> {
-    input: Box<Buffer + 'a>
-}
+fn lex<T: Buffer>(input: &mut T, callback: |&str, &[&str]|) -> Option<std::io::IoError> {
+    for maybe_line in input.lines() {
+        match maybe_line {
+            Ok(line) => {
+                let line = line.as_slice();
+                let line = line.split('#').next().unwrap();
 
-impl<'a> Lexer<'a> {
-    fn new(input: Box<Buffer + 'a>) -> Lexer<'a> {
-        Lexer { input: input }
-    }
-}
-
-impl<'a> Iterator<String> for Lexer<'a> {
-    fn next(&mut self) -> Option<String> {
-        match self.input.read_char() {
-            Ok(ch) => Some(ch.to_string()),
-            Err(_) => None
+                let mut words = line.words();
+                match words.next() {
+                    Some(header) => {
+                        let args: Vec<&str> = words.collect();
+                        callback(header, args.as_slice())
+                    }
+                    None => {}
+                }
+            }
+            Err(e) => { return Some(e); }
         }
     }
+    None
 }
 
 #[test]
 fn test_lex() {
-    let string = "Hello, world!".to_string();
+    let input = r#"
+   header0      arg0  arg1	arg2#argX   argX
+header1 arg0    arg1
+# Comment
+header2 Hello, world!
+"#;
 
-    let reader = std::io::MemReader::new(string.into_bytes());
-    let mut lexer = Lexer::new(box reader);
-
-    for token in lexer {
-        println!("{}", token);
-    }
+    lex(&mut input.as_bytes(), |header, args| {
+        match header {
+            "header0" => assert_eq!(args, ["arg0", "arg1", "arg2"]),
+            "header1" => assert_eq!(args, ["arg0", "arg1"]),
+            "header2" => assert_eq!(args, ["Hello,", "world!"]),
+            _ => panic!()
+        }
+    });
 }
