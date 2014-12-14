7859673d48ef75bd207887d1410da68850dfb885
Hyeon Kim <simnalamburt@gmail.com>
2014-12-14 21:55:49 +0900

    match header { }

--- src/lib.rs
+++ src/lib.rs
@@ -40,3 +40,63 @@ header2 Hello, world!
         }
     });
 }
+
+
+/// Parses a wavefront `.obj` file
+pub fn load<T: Buffer>(input: &mut T) {
+    lex(input, |header, args| {
+        match header {
+            // Vertex data
+            "v" => {}
+            "vt" => {}
+            "vn" => {}
+            "vp" => {}
+
+            // Free-form curve / surface attributes
+            "cstype" => {}
+            "deg" => {}
+            "bmat" => {}
+            "step" => {}
+
+            // Elements
+            "p" => {}
+            "l" => {}
+            "f" => {}
+            "curv" => {}
+            "curv2" => {}
+            "surf" => {}
+
+            // Free-form curve / surface body statements
+            "parm" => {}
+            "trim" => {}
+            "hole" => {}
+            "scrv" => {}
+            "sp" => {}
+            "end" => {}
+
+            // Connectivity between free-form surfaces
+            "con" => {}
+
+            // Grouping
+            "g" => {}
+            "s" => {}
+            "mg" => {}
+            "o" => {}
+
+            // Display / render attributes
+            "bevel" => {}
+            "c_interp" => {}
+            "d_interp" => {}
+            "lod" => {}
+            "usemtl" => {}
+            "mtllib" => {}
+            "shadow_obj" => {}
+            "trace_obj" => {}
+            "ctech" => {}
+            "stech" => {}
+
+            // Unexpected header
+            _ => {}
+        }
+    });
+}
