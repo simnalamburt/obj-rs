f6e04d2489e0c3fef3ca760ba5bc1f905a06e1dc
Hyeon Kim <simnalamburt@gmail.com>
2014-12-28 10:13:25 +0900

    test: Enable test codes

--- tests/obj.rs
+++ tests/obj.rs
@@ -1,3 +1,5 @@
+#![feature(macro_rules)]
+
 extern crate obj;
 
 use std::io::{BufferedReader, File};
@@ -7,5 +9,26 @@ fn test_obj() {
     let path = Path::new("tests").join("fixtures").join("cube.obj");
     let mut input = BufferedReader::new(File::open(&path));
 
-    // obj::obj(&mut input);
+    let obj = obj::obj(&mut input);
+
+    macro_rules! eq {
+        { $($lhs:expr $rhs:expr)* } => ({
+            $(
+                assert_eq!($lhs, $rhs);
+            )*
+        })
+    }
+
+    eq! {
+        obj.name                                    "Cube".to_string()
+
+        obj.vertices.len()                          8
+        obj.tex_coords.len()                        14
+        obj.normals.len()                           0
+        obj.param_vertices.len()                    0
+
+        obj.groups.len()                            1
+        obj.groups[0].meshes.len()                  1
+        obj.groups[0].meshes[0].polygons.len()      6
+    };
 }
