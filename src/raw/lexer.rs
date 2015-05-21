97145a175b08d8e502b10e1ac23c12ed91c33d27
Hyeon Kim <simnalamburt@gmail.com>
2015-05-21 11:04:59 +0900

    Backporting split_whitepace into Rust 1.0

--- src/raw/lexer.rs
+++ src/raw/lexer.rs
@@ -8,7 +8,14 @@ pub fn lex<T, F>(input: T, mut callback: F) -> ObjResult<()>
         let line = try!(line);
         let line = line.split('#').next().unwrap();
 
-        let mut words = line.split_whitespace();
+        // Backporting Rust 1.1 into 1.0
+        fn is_not_empty(s: &&str) -> bool { !s.is_empty() }
+        let is_not_empty: fn(&&str) -> bool = is_not_empty; // coerce to fn pointer
+
+        fn is_whitespace(c: char) -> bool { c.is_whitespace() }
+        let is_whitespace: fn(char) -> bool = is_whitespace; // coerce to fn pointer
+
+        let mut words = line.split(is_whitespace).filter(is_not_empty);
         match words.next() {
             Some(stmt) => {
                 let args: Vec<&str> = words.collect();
