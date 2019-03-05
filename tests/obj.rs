2e9eefe54f3678f36b81f5fcf3dcac17ae57ab18
Richard M <richard.meester@gmail.com>
2019-03-04 21:09:14 -0800

    Add a textured vertex to support textured meshes

--- tests/obj.rs
+++ tests/obj.rs
@@ -54,3 +54,37 @@ fn dome() {
     assert_eq!(obj.indices[1], 2);
     assert_eq!(obj.indices[2], 6);
 }
+
+#[test]
+fn textured_cube() {
+    let obj: Obj<TexturedVertex, u32> = load_obj(fixture!("textured-cube.obj")).unwrap();
+
+    macro_rules! vt {
+        (($($p:expr),*), ($($n:expr),*), ($($t:expr),*)) => ({
+            TexturedVertex {
+                position: [$(stringify!($p).parse::<f32>().unwrap()),*],
+                normal: [$(stringify!($n).parse::<f32>().unwrap()),*],
+                texture: [$(stringify!($t).parse::<f32>().unwrap()),*]
+            }
+        })
+    }
+
+    assert_eq!(obj.name, Some("cube".to_string()));
+    assert_eq!(obj.vertices.len(), 24);
+    dbg!(&obj.vertices);
+    assert_eq!(obj.vertices[0], vt!((-0.500000, -0.500000, 0.500000), (0.000000, 0.000000, 1.000000), (0.000000, 0.000000, 0.000000)));
+    assert_eq!(obj.vertices[1], vt!((0.500000,  -0.500000, 0.500000), (0.000000, 0.000000, 1.000000), (1.000000, 0.000000, 0.000000)));
+    assert_eq!(obj.vertices[2], vt!((-0.500000,  0.500000, 0.500000), (0.000000, 0.000000, 1.000000), (0.000000, 1.000000, 0.000000)));
+    assert_eq!(obj.vertices[3], vt!((0.500000,  0.500000, 0.500000), (0.000000, 0.000000, 1.000000), (1.000000, 1.000000, 0.000000)));    
+    assert_eq!(obj.vertices[4], vt!((-0.500000,  0.500000, 0.500000), (0.000000, 1.000000, 0.000000), (0.000000, 0.000000, 0.000000)));
+    assert_eq!(obj.vertices[5], vt!((0.500000,  0.500000, 0.500000), (0.000000, 1.000000, 0.000000), (1.000000, 0.000000, 0.000000)));    
+    assert_eq!(obj.indices.len(), 36);
+    assert_eq!(obj.indices[0], 0);
+    assert_eq!(obj.indices[1], 1);
+    assert_eq!(obj.indices[2], 2);
+    assert_eq!(obj.indices[3], 2);
+    assert_eq!(obj.indices[4], 1);
+    assert_eq!(obj.indices[5], 3);
+    assert_eq!(obj.indices[6], 4);
+    assert_eq!(obj.indices[7], 5);
+}
